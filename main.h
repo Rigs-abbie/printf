1ebd5adb8ed48db4770a2f48d2f867f12600b00b
abbie <abigailnjoki001@gmail.com>
2023-08-22 04:15:14 -0700

    print_37.c

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_str(char *str);
+int print_int(int num);
+int print_char(char c);
+int print_37(char c);
+
+
+#endif
