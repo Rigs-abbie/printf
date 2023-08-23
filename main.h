b60313a1ae3e216600236c50b397ac7d5fe7539f
Hellen Maseko <masekohelen@gmail.com>
2023-08-23 02:37:30 -0700

    get_width.c

--- main.h
+++ main.h
@@ -17,6 +17,8 @@ int _printf(const char *format, ...);
 int our_putchar(char c);
 int putss(char *c);
 int _putchar(char c);
+int get_width(const char *format, int *i, va_list list);
+int is_digit(char);
 int get_flags(const char *format, int *i);
 int print_str(char *str);
 int print_int(int num);
