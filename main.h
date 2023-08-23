2eb6d7e4e14e4a7a156172f1466f9393e5dace94
Hellen Maseko <masekohelen@gmail.com>
2023-08-23 01:34:22 -0700

    flags.c

--- main.h
+++ main.h
@@ -6,10 +6,18 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
 int _printf(const char *format, ...);
 int our_putchar(char c);
 int putss(char *c);
 int _putchar(char c);
+int get_flags(const char *format, int *i);
 int print_str(char *str);
 int print_int(int num);
 int print_char(char c);
